84d8c5c8f5273a930bf7eaa8f1b0063d4a87e089
Therese Cecilia Johansson <Theresececilias@gmail.com>
2024-01-15 15:14:04 +0100

    Git init. Add google fonts, grid layout and theme data.

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meal_app/categories.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 159, 216, 203),
+  ),
+  textTheme: GoogleFonts.latoTextTheme().copyWith(
+    bodyLarge: const TextStyle(color: Colors.white),
+    bodyMedium: const TextStyle(color: Colors.white),
+  ),
+);
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoryScreen(),
+    );
+  }
+}
